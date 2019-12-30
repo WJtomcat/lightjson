6830e279dc4b79823275c5b950412360634511eb
wangju <wangju@ainnovation.com>
2019-12-30 15:23:17 +0800

    Initial Commit

---
+++ lightjson/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lightjson
+//
+//  Created by Admin on 2019/12/30.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
